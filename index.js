530dabb39382e32c03fb89349e695657d8f228fa
Edgar Vargas <vargased23@gmail.com>
2023-03-20 14:45:28 -0700

    added package.json and index.s file and starter code

---
+++ index.js
@@ -0,0 +1,6 @@
+// imoort modules/packages
+const inquirer = require('inquirer');
+const connection = require('./config/connection');
+const Department = require('./lib/Department');
+const Role = require('./lib/Role');
+const Employee = require('./lib/Employee');
